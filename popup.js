e4262a97d6472de98cbff0a29f3969423dd474f7
Alvaro Canseco-Martinez <alvcm1501@gmail.com>
2025-09-27 11:40:37 -0400

    Added hover feature

--- popup.js
+++ popup.js
@@ -10,17 +10,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if(query === ""){
             messages.textContent = "Please Enter Some Text!";
+            messages.style.color = "red";
+            messages.style.font = "13px courier";
+            messages.style.padding = "10px";
         } else {
             const p = document.createElement("p");
             p.textContent = `You Typed: ${query}`;
             p.style.color = "white";
+            p.style.font = "13px courier";
             p.style.padding = "10px";
             messages.appendChild(p);
             inputBox.value = "";
         }
+     }
+
+   
+    function addToHistory() {
+        const query = searchButton.value.trim();
+        if (query !== "") {
+        const p = document.createElement("p");
+        p.textContent = `History: ${query}`;
+        p.style.color = "yellow";
+        messages.appendChild(p);
+        inputBox.value = "";
+        }
     }
 
     searchButton.addEventListener("click", handleSearch);
+    messages.addEventListener("click", addToHistory);
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         handleSearch();
